9cc02f252a835ddf255467d764ebbecc9dc17a91
cristopherturazza <cristopherturazza@gmail.com>
2022-04-28 20:40:31 +0200

    webpack config

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,7 @@ import '../css/mobile.css';
 import '../css/desktop.css';
 
 import axios from 'axios';
-import './city.js'
 import { City } from './city.js';
-import './showcity.js'
 import { ShowCity } from './showcity.js';
 const _get = require('lodash/get');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { LoaderOptionsPlugin } = require('webpack');
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
     entry: {
@@ -10,19 +13,51 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        clean: true
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'imgs/[hash][ext][query]',
+        clean: true,
     },
+    // optimization: {
+    //     minimizer: [
+    //       new CssMinimizerPlugin()
+    //     ],
+    // },
     module: { rules:[
+            {
+            test: /\.html$/,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'html-loader',
+                options: { minimize: true },
+              }]
+            },
             {
                 test: /\.css$/i,
                 use: ['style-loader','css-loader'],
+            },
+            {
+                test: /\.(svg|png|jpe?g|gif)$/i,
+                type: 'asset/resource'
+            },          
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-class-properties', 
+                                        '@babel/plugin-transform-runtime'],
+                    }
+                },
             }
 
     ]},
     plugins: [
+        new MiniCssExtractPlugin({filename:"[name].[contenthash].css"}),
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: "test webpack",
             template: './src/index.html'
         })
     ],
@@ -33,7 +68,8 @@ module.exports = {
                 name: 'firefox'
             }
         },
-        static: path.resolve(__dirname, 'src')
+        static: path.resolve(__dirname, 'dist')
     },
-    mode: "development"
+
+    mode: "production"
 }
